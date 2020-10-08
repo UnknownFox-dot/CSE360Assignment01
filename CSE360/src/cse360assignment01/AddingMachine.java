14f04cb405a2663e8371727744fd115699a6a16b
Kevin <Kevin@Lenovo-PC>
2020-10-07 19:04:27 -0700

    everything but toString() sorted
    Due to finding no way to get around zero inputs to have toString() print
    out a single line, it was simply removed and ignored.

--- CSE360/src/cse360assignment01/AddingMachine.java
+++ CSE360/src/cse360assignment01/AddingMachine.java
@@ -2,34 +2,43 @@ package cse360assignment01;
 
 public class AddingMachine
 {
-  private int total;
-  
-  public AddingMachine ()
+  private static int total;
+
+  public AddingMachine()
   {
     total = 0; 
   }
   
-  public int getTotal ()	//returns the overall total
+  public static void main (String [] args) 
+  {
+	  add(5);
+	  subtract(7);
+	  add(9);
+	  System.out.println("0 + 5 - 7 + 9");
+	  System.out.println("Total: " + getTotal());
+	  subtract(6);
+	  System.out.println("0 + 5 - 7 + 9 - 6");
+	  System.out.println("Total: " + getTotal());
+	  clear();
+	  System.out.println("Total: " + getTotal());
+  }
+  
+  public static int getTotal()	//returns the overall total
   {
     return total;
   }
   
-  public void add (int value)	//performs the add function
+  public static void add(int value)	//performs the add function
   {
 	  total += value;
   }
 
-  public void subtract (int value)	//performs the subtraction function
+  public static void subtract(int value)	//performs the subtraction function
   {
 	  total -= value;
   }
-
-  public String toString ()	//returns the operations performed
-  {
-    return "";
-  }
-
-  public void clear()	//resets the total
+  
+  public static void clear()	//resets the total
   {
 	  total = 0;
   }
